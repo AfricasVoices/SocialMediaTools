187ad9cdca46858c90fd89ff37fcb67a34b0c40a
Alexander Simpson <as2388@cam.ac.uk>
2021-02-23 10:58:29 +0000

    Version bump for release v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SocialMediaTools",
-    version="0.1.1",
+    version="0.1.2",
     python_requires='>=3.6.0',
     url="https://github.com/AfricasVoices/SocialMediaTools",
     packages=find_packages(exclude=("test",)),
